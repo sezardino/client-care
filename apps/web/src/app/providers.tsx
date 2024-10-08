7fc2cd9410871d5f054fcac4ef178e156f5df9bd
Edward <e.arechwa@gmail.com>
2024-10-08 17:19:38 +0200

    ft: add auth pages

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/react";
 import { PropsWithChildren } from "react";
 
 export const ClientProviders = (props: PropsWithChildren) => (
-  <NextUIProvider>{props.children}</NextUIProvider>
+  <ClerkProvider>
+    <NextUIProvider>{props.children}</NextUIProvider>
+  </ClerkProvider>
 );
--- apps/web/src/const/url.ts
+++ apps/web/src/const/url.ts
@@ -1,4 +1,10 @@
 export const ProjectUrls = {
   home: "/",
   roadMap: "/road-map",
+
+  // auth
+  login: "/auth/login",
+  registration: "/auth/registration",
+  registrationVerification: "/auth/registration-verification",
+  forgotPassword: "/auth/forgot-password",
 };
