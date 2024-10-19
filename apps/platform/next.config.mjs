0675b19c16a07395ad76616d3d2e5321aa32665c
Edward <e.arechwa@gmail.com>
2024-10-19 11:31:50 +0200

    cors

--- apps/platform/next.config.mjs
+++ apps/platform/next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/widget/:path*",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
