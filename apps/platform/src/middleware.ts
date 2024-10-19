0445a521861779de576db0133277733da5d426f7
Edward <e.arechwa@gmail.com>
2024-10-19 11:53:23 +0200

    fix: middleware cors

--- apps/platform/src/middleware.ts
+++ apps/platform/src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 import { ProjectUrls } from "./const/url";
 
 const isPublicRoute = createRouteMatcher([
@@ -9,6 +10,22 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    const res = NextResponse.next();
+
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT,OPTIONS"
+    );
+    res.headers.append(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Authorisation, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    return res;
+  }
+
   if (!isPublicRoute(req)) {
     auth().protect();
   }
