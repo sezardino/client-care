ed32e2fa2765d963f851dcbcc174f543c368bc67
Edward <e.arechwa@gmail.com>
2024-10-19 15:04:50 +0200

    rf: add dev prop to widget

--- apps/platform/src/app/layout.tsx
+++ apps/platform/src/app/layout.tsx
@@ -45,13 +45,14 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
           </HydrationBoundary>
         </ClientProviders>
 
-        {process.env.NODE_ENV === "production" && (
-          <>
-            {/* @ts-ignore */}
-            <feedback-widget token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiJjbTJhbXJzdXgwMDAwaHdlN2sya2ppOWd6Iiwib3JnYW5pemF0aW9uSWQiOiJjbTI4MDd5ZHAwMDAzZzZzNGZydGR1czNmIiwid2lkZ2V0SWQiOiJjbTJkaDAzZm0wMDAwMTE1YmhqdWd1OGFuIiwiaWF0IjoxNzI5MzI3OTQ1LCJleHAiOjMzMjU1MzcwMzQ1fQ.4D9fyEXxBO5ZOsrDQWULszZ9RRU70sN9cPN7K5KWPQA"></feedback-widget>
-            <Script src="https://client-care-widget-dev.vercel.app/widget.umd.js" />
-          </>
-        )}
+        {/* @ts-ignore */}
+        <feedback-widget
+          dev={process.env.NODE_ENV !== "production" ? "true" : undefined}
+          token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiJjbTJhbXJzdXgwMDAwaHdlN2sya2ppOWd6Iiwib3JnYW5pemF0aW9uSWQiOiJjbTI4MDd5ZHAwMDAzZzZzNGZydGR1czNmIiwid2lkZ2V0SWQiOiJjbTJkaDAzZm0wMDAwMTE1YmhqdWd1OGFuIiwiaWF0IjoxNzI5MzI3OTQ1LCJleHAiOjMzMjU1MzcwMzQ1fQ.4D9fyEXxBO5ZOsrDQWULszZ9RRU70sN9cPN7K5KWPQA"
+        >
+          {/* @ts-ignore */}
+        </feedback-widget>
+        <Script src="https://client-care-widget-dev.vercel.app/widget.umd.js" />
       </body>
     </html>
   );
