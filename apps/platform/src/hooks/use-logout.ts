a636f387619015eb9f22db5c1dc64d85c57fd23d
Edward <e.arechwa@gmail.com>
2024-10-19 12:54:51 +0200

    ft: clear cache after logout

--- apps/platform/src/hooks/use-logout.ts
+++ apps/platform/src/hooks/use-logout.ts
@@ -1,19 +1,22 @@
 import { ProjectUrls } from "@/const/url";
 import { useClerk } from "@clerk/nextjs";
+import { useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
 import { toast } from "sonner";
 
 export const useApplicationLogout = () => {
   const { signOut } = useClerk();
+  const queryClient = useQueryClient();
 
   const logout = useCallback(async () => {
     try {
-      signOut({ redirectUrl: ProjectUrls.home });
+      await signOut({ redirectUrl: ProjectUrls.login });
+      queryClient.clear();
       toast.success("You are logged out successfully");
     } catch {
       toast.error("Something went wrong");
     }
-  }, [signOut]);
+  }, [queryClient, signOut]);
 
   return { logout };
 };
